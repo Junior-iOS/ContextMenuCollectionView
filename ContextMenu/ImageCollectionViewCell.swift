858c8ce723e222c27333631122ef39a2f84a078f
Nelson Junior <nj.development83@gmail.com>
2021-11-26 01:26:52 -0300

    App created

---
+++ ContextMenu/ImageCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  ImageCollectionViewCell.swift
+//  ContextMenuInCollection
+//
+//  Created by Junior Silva on 25/11/21.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    static let identifier = "ImageCollectionViewCell"
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.frame = contentView.bounds
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+    }
+}
--- ContextMenu/ViewController.swift
+++ ContextMenu/ViewController.swift
@@ -1,19 +1,92 @@
 //
 //  ViewController.swift
-//  ContextMenu
+//  ContextMenuInCollection
 //
-//  Created by Junior Silva on 26/11/21.
+//  Created by Junior Silva on 25/11/21.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 2
+        layout.minimumInteritemSpacing = 2
+        
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .systemGray6
+        collection.register(ImageCollectionViewCell.self, forCellWithReuseIdentifier: ImageCollectionViewCell.identifier)
+        
+        return collection
+    }()
+    
+    let imageNames = Array(1...6).map { "hacker\($0)" }
+    var favorites = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        collectionView.frame = view.bounds
+    }
 
 }
 
+// MARK: - DELEGATES and DATASOURCE
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.identifier, for: indexPath) as? ImageCollectionViewCell else { fatalError() }
+        cell.imageView.image = UIImage(named: imageNames[indexPath.row])
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (view.frame.size.width / 2) - 4, height: (view.frame.size.width / 2) - 4)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        let config = UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { [weak self] _ in
+            
+            let copy = UIAction(title: "Copy", image: UIImage(systemName: "link"), state: .off) { _ in
+                print("Tapped Copy")
+            }
+            
+            let title = self?.favorites.contains(indexPath.row) == true ? "Remove Favorite" : "Favorite"
+            let image = self?.favorites.contains(indexPath.row) == true ? UIImage(systemName: "star.fill") : UIImage(systemName: "star")
+            
+            let favorite = UIAction(title: title, image: image, state: .off) { [weak self] _ in
+                if self?.favorites.contains(indexPath.row) == true {
+                    self?.favorites.removeAll(where: { $0 == indexPath.row })
+                } else {
+                    self?.favorites.append(indexPath.row)
+                }
+            }
+            
+            let search = UIAction(title: "Search", image: UIImage(systemName: "magnifyingglass"), state: .off) { _ in
+                print("Tapped search")
+            }
+            
+            return UIMenu(title: "Actions",
+                          image: nil,
+                          identifier: nil,
+                          options: UIMenu.Options.displayInline,
+                          children: [copy, favorite, search]
+            )
+        }
+        
+        return config
+    }
+}
